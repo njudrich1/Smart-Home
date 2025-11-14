97ac4a73a87e54be335dcb7a374a242713e8acda
njudrich1 <njudrich1@live.co.uk>
2025-11-14 15:14:23 +0000

    Code to calculate CRC-16 XModem and code to check whether the data message has been corrupted. Include tests to confirm what happens if the data message is corrupt.

--- Crc/crc.c
+++ Crc/crc.c
@@ -5,14 +5,18 @@
 
 uint16_t crc16(uint8_t data, int nbytes)
 {
+    /* Old Design*/
+
     uint16_t crc = 0;
-    uint16_t data_16 =  (uint16_t)data;
+    //uint16_t data_16 =  (uint16_t)data;
     uint16_t check_msb = 0;
 
     for(int byte = 0; byte < nbytes; byte++)
     {
         printf("Round: %d:\n", byte);
         printf("1: %x\n", crc);
+        printf("Byte: %x\n", data);
+        uint16_t data_16 =  (uint16_t)data;
         printf("1.5: %x\n", (data_16 << 8)); // 0x3100 (11000100000000)
 
         crc = crc ^ (data_16 << 8);
@@ -42,23 +46,200 @@ uint16_t crc16(uint8_t data, int nbytes)
     return crc;
 }
 
-// Main function: entry point for execution
+void print_msg(uint8_t *data, int length)  // DEBUG 
+{
+    printf("Message: ");
+    int msg_len = length;
+    for(int byte = 0; byte < msg_len; byte++)
+    {
+        printf("%x ", data[byte]);
+    }
+    printf("\n");
+}
+
+uint16_t evaluate_crc_from_msb(uint16_t crc)
+{
+    //printf("BEGIN: evaluate_crc_from_msb ------------------------------------\n"); // DBG
+    uint16_t crc_data_byte = crc;
+    uint16_t check_msb = crc_data_byte & 0x8000;
+    //printf("MSB: %#x\n", check_msb); // DBG
+    if (check_msb)
+    {
+        crc_data_byte = crc_data_byte << 1;
+        //printf("1: %x\n", crc_data_byte); // DBG
+        crc_data_byte = crc_data_byte ^ 0x1021; 
+        //printf("1.2: %x\n", crc_data_byte); // DBG
+    }
+    else
+    {
+        //printf("2: %x\n", crc_data_byte << 1); // DBG
+        crc_data_byte = crc_data_byte << 1;
+    }
+    //printf("END: evaluate_crc_from_msb ------------------------------------\n\n"); // DBG
+
+    return crc_data_byte;   
+}
+
+uint16_t calc_crc_from_one_byte(uint16_t init_crc)
+{
+    //printf("\nBEGIN: calc_crc_from_one_byte ------------------------------------\n"); // DBG
+    uint16_t data_byte = init_crc;
+    uint16_t crc = 0x0000;
+    
+    for(unsigned char bit = 0; bit < 8; bit++)
+    {
+        //printf("Bit %d:\n", bit); // DBG
+        data_byte = evaluate_crc_from_msb(data_byte);
+    }
+
+    crc = data_byte;
+    // printf("END: calc_crc_from_one_byte ------------------------------------\n\n"); // DBG
+
+    return crc;
+}
+
+
+uint16_t crc16_multi_byte(uint8_t *data, int nbytes)
+{
+    /* Refactored Design */
+
+    printf("\n------------------------------------ BEGIN: crc16_multi_byte ------------------------------------\n");
+    uint16_t crc_result = 0x0000;
+    uint16_t temp_crc = 0x0000;
+
+    for(int byte = 0; byte < nbytes; byte++)
+    {
+        //printf("1:"); // DBG
+        //printf("Byte Number: %d:\n", byte); // DBG
+        //printf("Current CRC value: %x\n", temp_crc); // DBG
+        //printf("Byte Value: %x\n", data[byte]); // DBG
+
+        uint16_t data_16 =  (uint16_t)data[byte];
+
+        //printf("1.5: %x\n", (data_16 << 8)); // DBG
+
+        temp_crc = temp_crc ^ (data_16 << 8);
+
+        //printf("2: %x\n", temp_crc); // DBG
+
+        temp_crc = calc_crc_from_one_byte(temp_crc);
+    }
+    crc_result = temp_crc;
+    printf("------------------------------------ END: crc16_multi_byte ------------------------------------\n\n");
+
+    return crc_result;
+}
+
+// USER TODO: Uncomment the desired operation to perform. Then uncomment a byte length
+#define CALC_CRC
+//#define CHECK_UCORRPT_MSG
+//#define CHECK_CORRPT_MSG
+
+// USER TODO: Select the byte msg length
+#define ONE_BYTE_MSG
+//#define FOUR_BYTE_MSG
+
+
 int main() 
 {
-    // crc = 0001000000100001 (0x1021)
-    // data = 00110001 (0x31) (ASCII character '1')
 
-    // 2)
-    uint8_t y = 0x32;
-    int byte_num = 1;
+#ifdef CALC_CRC
+    printf("Calculate Message CRC: ------------------------------------\n");
+    #ifdef ONE_BYTE_MSG
+        int message_len = 1;
+    #else 
+        int message_len = 4; // FOUR_BYTE_MSG
+    #endif
+#endif
+
+#ifndef CALC_CRC // Checking msg and crc
+    // msg length + crc
+    #ifdef ONE_BYTE_MSG
+        int message_len = 3; 
+    #else 
+        int message_len = 6; // FOUR_BYTE_MSG
+    #endif
+#endif
+
+
+#ifdef CHECK_UCORRPT_MSG
+    printf("Check Uncorrupt Message CRC: ------------------------------------\n");
+#endif
+
+#ifdef CHECK_CORRPT_MSG
+    printf("Check Corrupt Message CRC: ------------------------------------\n");
+#endif
+
+#ifdef CALC_CRC
+    // Calculate CRC of message //
+#ifdef ONE_BYTE_MSG
+    /*
+       - message = 00110001 (0x31) (ASCII character '1')
+       - Expected CRC = 0x2672 (0010_0110_0111_0010)
+    */
+    uint8_t message[1] = {0x31}; 
+#endif
+
+#ifdef FOUR_BYTE_MSG
+    /*
+       - message = 0x31, 0x32, 0x33, 0x34 (ASCII character '1', '2', '3', '4')
+       - Expected CRC = 0xd789
+    */
+    uint8_t message[4] = {0x31, 0x32, 0x33, 0x34};
+#endif
+
+#endif
+
+#ifdef CHECK_UCORRPT_MSG
+    // Check data has not been corrupted //
+    // Non Corrupted Data with Crc//
+
+#ifdef ONE_BYTE_MSG
+    // 1 byte message not corrupted with it's crc. Should return zero.
+    uint8_t message[1+2] = {0x31, 0x26, 0x72}; 
+#endif
+
+#ifdef FOUR_BYTE_MSG
+    // 4 byte message not corrupted with it's crc. Should return zero.
+    uint8_t message[4+2] = {0x31, 0x32, 0x33, 0x34, 0xd7, 0x89}; 
+#endif
+
+#endif
+
+#ifdef CHECK_CORRPT_MSG
+    // Corrupted Data with Crc //
+#ifdef ONE_BYTE_MSG
+    // (1)
+    /* 1 byte message corrupted 
+       - With LSB bit flip (to 0).
+       - 0x31 -> 0x30 (0011_0001 -> 0011_0000)
+       - Output: 0011_0111_0011_0000 (0x3730) != 0
+
+    */ 
+    uint8_t message[1+2] = {0x30, 0x26, 0x72};
+#endif
+
+#ifdef FOUR_BYTE_MSG
+    // (2)
+    /* 4 byte message corrupted 
+       - 3rd byte corrupted with 3rd bit flip (0 to 1)
+       - 0x33 -> 0x37 (0011_0011 -> 0011_0111)
+       - Crc with uncorrupt message is 1101_0111_1000_1001 (0xd789)
+       - Output: 1100_1010_1111_0001 (0xcaf1) != 0
+    */ 
+    uint8_t message[4+2] = {0x31, 0x32, 0x37, 0x34, 0xd7, 0x89}; 
+#endif
+#endif
+
     uint16_t result = 0;
-    printf("Data in: %#x\n", y);
+    // Poly = 0001000000100001 (0x1021)
 
-    result = crc16(y, byte_num);
+    print_msg(message, message_len); //DEBUG
+
+    //result = crc16(y, byte_num);  // Old crc calculating function
+    result = crc16_multi_byte(message, message_len);
 
     printf("Data out: %#x\n", result);
-    printf("Size of y: %d\n", (int)sizeof(y));
-    printf("Size of result: %d\n", (int)sizeof(result));
 
     return 0;
 }
